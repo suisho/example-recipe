d89ed227d747a7b20a673c17e5e8493de28d3333
suisho <suisho000000@gmail.com>
2014-06-07 00:42:44 +0900

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of example-recipe
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'example-recipe'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures example-recipe'
+long_description 'Installs/Configures example-recipe'
+version          '0.1.0'
+
